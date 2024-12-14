a0ba69ba69bdf2bbe5adf0861907c814fd7df6b4
N.NAVATEJA REDDY <navatejar80@gmail.com>
2024-12-14 11:13:24 +0530

    Add files via upload

---
+++ stdin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+String myString = scanner.next();
+int myInt = scanner.nextInt();
+int myInt2=scanner.nextInt();
+scanner.close();
+
+System.out.println( myString);
+System.out.println(myInt);
+System.out.println(myInt2);
+    }
